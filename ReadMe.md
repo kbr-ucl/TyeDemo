47313f118546a669139795bcb40c6d3e1232645b
Kaj Bromose <kbr@ucl.dk>
2020-11-11 11:58:05 +0100

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# **Frontend Backend sample with tye run **
+This is an implementation of the "Frontend Backend sample with tye run" example found here:
+[Source](https://github.com/dotnet/tye/blob/master/docs/tutorials/hello-tye/00_run_locally.md)
+
+First - follow the [Getting Started](https://github.com/dotnet/tye/blob/master/docs/getting_started.md)
+
+Then (in a console window in the TyeDemo folder) type
+
+ `tye run`
+
+Then open [tye dashboard](localhost:8000)
+
+Then at the fronend binding press the https link, and watch the data fetched at the backend being displayed at the frontend
+
+
+
---
+++ frontend/Infrastructure/ServiceProxy/WeatherClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using frontend.ViewModels;
+
+namespace frontend.Infrastructure.ServiceProxy
+{
+    public class WeatherClient
+    {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        private readonly HttpClient client;
+
+        public WeatherClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherAsync()
+        {
+            var responseMessage = await this.client.GetAsync("/weatherforecast");
+            var stream = await responseMessage.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<WeatherForecast[]>(stream, options);
+        }
+    }
+}
